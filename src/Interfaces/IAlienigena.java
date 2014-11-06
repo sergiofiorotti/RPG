7aaa70f90f93e02d92960a8fb8c54d19e1e69afd
Sérgio Fiorotti <sergio@sfdesenvolvimento.com.br>
2014-11-06 14:10:53 -0200

    IClasse criada!

--- src/Interfaces/IAlienigena.java
+++ src/Interfaces/IAlienigena.java
@@ -1,4 +1,4 @@
 package Interfaces;
 
-public interface IAlienigena {
+public interface IAlienigena extends IClasse {
 }
--- src/Interfaces/IHumano.java
+++ src/Interfaces/IHumano.java
@@ -1,5 +1,5 @@
 package Interfaces;
 
-public interface IHumano{
-
+public interface IHumano extends IClasse{
+	
 }
